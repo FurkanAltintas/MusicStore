c1ad10aec8c73d5dc0c6f8d3cfa9a21f7e4b8fd6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-01-20 16:07:11 +0300

    Add project files.

---
+++ MusicStore.Models/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.Models.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(250, MinimumLength = 3, ErrorMessage ="Şartlara uygun bir değer giriniz")]
+        public string Name { get; set; }
+    }
+}

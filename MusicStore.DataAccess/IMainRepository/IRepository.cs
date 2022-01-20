835be615df3f8f0552b026e0f1bc501e34230754
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-01-20 16:37:19 +0300

    Repository Added

---
+++ MusicStore.DataAccess/IMainRepository/IRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.DataAccess.IMainRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        // Generic tipler T tipinde belirtilir (yazılı olmayan bir kural)
+        // Bunu nerede kullanıcaksak kullanalım Repository tanımlarken vereceğimiz tip class olmak zorunda
+
+        T Get(int id); // id değerine göre T değerini getir (Tek bir tane getirir)
+
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>,
+                              IOrderedQueryable<T>> orderBy = null,
+                              string includeProperty = null);
+
+        // Expression<Func<T, bool>> filter = null, (bir filtreleme ayarladık)
+        // Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null (sıralama yaptık)
+        //  Filtreleme yapabiliriz, sıralayabiliriz ve o tablonun ilişkili olduğu tablonunda değerlerini getirmemiz gerekebilir.
+
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null);
+
+        void Add(T entity); // Ekleme
+
+        void Remove(int id); // Kayıt silme id değerine göre
+
+        void Remove(T entity); // Kayıt silme
+
+        void RemoveRange(IEnumerable<T> entities); // Çoklu Silme
+    }
+}
---
+++ MusicStore.DataAccess/MainRepository/Repository.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicStore.DataAccess.Data;
+using MusicStore.DataAccess.IMainRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.DataAccess.MainRepository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            this._dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public T Get(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperty = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter); // filter boş değilse filterı uygula
+            }
+
+            if (includeProperty != null)
+            {
+                foreach (var item in includeProperty.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)) // split ile virgülleri ayırıyoruz ve boş datalar var ise sil diyoruz (RemoveEmptyEntries)
+                {
+                    query = query.Include(item);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+
+            return query.ToList();
+
+            /* IQueryable sorgularımızı hazırlıyor ama database e aktarmıyor.
+             * ToList dediğimiz vakit sorgunun sonucunu ister
+             * Her defasında ToList deseydik kodu çalıştırıp database e gidecekti. Bunu her işlemde yapıcaktı ve database e 4 defa git gel yapıcaktı. IQueryable kullanarak kodumuzu bir kere çalıştırıp 1 kere yolluyoruz.
+             */
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var item in includeProperties.Split(new char[] {','},StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(item);
+                }
+            }
+
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            _dbSet.Remove(Get(id));
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
